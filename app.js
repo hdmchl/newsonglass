951e45dfd1fff5ccd9359389e5c4a1e247d86589
Hadi Michel Salem <hsalem@deloitte.com.au>
2014-04-05 16:30:41 +1100

    Start building out the node-app

---
+++ app.js
@@ -0,0 +1,41 @@
+/*
+ * @author: @hadi_michael
+ * @date: April 2014
+ */
+
+/* include and setup required packages */
+var appName         = 'NewsOnGlass',
+    express         = require('express'),
+    app             = express(),
+    http            = require('http'),
+    server          = http.createServer(app),
+    googleapis      = require('googleapis'),
+    OAuth2Client    = googleapis.OAuth2Client,
+    app_creds       = require('./server/app_credentials'), //see app_credentials__template.js
+
+/***** HANDLE Uncaught Exceptions *****/
+process.on('uncaughtException', function(err) {
+    console.error(err.stack);
+});
+/***** /HANDLE Uncaught Exceptions *****/
+
+/***** SETUP AND START SERVER *****/
+// configure the server
+app.configure(function(){
+    app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
+});
+
+// start the server
+try {
+    app.set('port', 8081);
+    server.listen(app.get('port'));
+    console.log('Server is listening on port: ' + app.get('port'));
+} catch (err) {
+    console.error('Server didn\'t start: \n' + err);
+}
+/***** /SETUP AND START SERVER *****/
---
+++ server/app_credentials__template.js
@@ -0,0 +1,15 @@
+/*
+ * @author: @hadi_michael
+ * @date: April 2014
+ */
+
+//TODO: 1. Plug in your Mirror API credentials below: client id, client secret, redirect url
+//      2. Rename this file to: app_credentials.js
+
+exports.getOAuthCreds = function (appName) {
+    return {
+        CLIENT_ID: '',   // TODO: YOUR CLIENT ID AS A STRING */
+        CLIENT_SECRET: '', // TODO: YOUR CLIENT SECRET AS A STRING */
+        REDIRECT_URL: '' // TODO: YOUR REDIRECT URL AS A STRING */
+    };
+}
