39df9276022f7f4e57c227eeb39b9b8ab15b04bf
Hadi Michael <hadimichael@gmail.com>
2014-05-11 19:47:27 +1000

    Point front-end to localhost

--- front-end/app/scripts/services/preferences.js
+++ front-end/app/scripts/services/preferences.js
@@ -4,13 +4,13 @@ angular.module('newsApp')
   .service('Preferences', function Preferences($http) {
     return {
       getPreferences: function(userId) {
-        return $http.get('http://newsonglass.herokuapp.com/user/'+userId+'/preferences').success(function(response) {
+        return $http.get('http://localhost:8080/user/'+userId+'/preferences').success(function(response) {
           console.log('GET results:', response);
           return response.data;
         });
       },
       postPreferences: function(userId, prefs) {
-        return $http.post('http://newsonglass.herokuapp.com/user/'+userId+'/preferences', prefs).success(function(response) {
+        return $http.post('http://localhost:8080/user/'+userId+'/preferences', prefs).success(function(response) {
           console.log('POST', prefs);
           console.log('POST response', response);
           return response;
--- front-end/app/scripts/services/user.js
+++ front-end/app/scripts/services/user.js
@@ -4,7 +4,7 @@ angular.module('newsApp')
   .service('User', function User($http) {
     return {
       getUser: function() {
-        return $http.get('http://newsonglass.herokuapp.com/user').then(function(result) {
+        return $http.get('http://localhost:8080/user').then(function(result) {
           console.log('user', result);
           return result.data;
         });
